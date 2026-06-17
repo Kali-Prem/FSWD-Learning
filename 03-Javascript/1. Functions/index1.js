4623bf0a73b8b6b8471361547a5ac6c5b22eb5d6
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-06-17 16:41:53 +0530

    Functions basics added

---
+++ 03-Javascript/1. Functions/index1.js
@@ -0,0 +1,35 @@
+one();
+console.log(a);
+var a = 10;
+var b = 199;
+console.log(a);
+function one(){
+    console.log(a);
+    var a = 99;
+    console.log(b);
+    b = 200;
+}
+console.log(b);
+one();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03-Javascript/FSD-15/0.Basic/index.js
@@ -0,0 +1,17 @@
+// let 123; //error
+let abc123 = 10;  //run
+console.log(abc123); //javascript is case-sensitive
+// let 12abc = 10; //error
+// let first wame = " sumit"
+
+// Only two this can be run
+let $ = 50;  //run
+let _ = 70;  //run
+
+
+
+// Primitive Datatype     [Number, string, boolean, null , undefined, symbol, bigint]
+// Non-Primitive Datatype [Objects, array, functions ]
+let a = null;
+let b = 1243;
+let c = undefined;
